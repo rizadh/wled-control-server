ff3a30299f4d937e0bbbdd0cbdc99d5f3581d097
Rizadh Nizam <me@rizadh.com>
2025-05-24 04:34:23 -0400

    Prevent race conditions with simultaneous server events

--- Server.ts
+++ Server.ts
@@ -26,45 +26,47 @@ export default class Server {
   }
 
   #getState() {
-    return this.#mutex.runExclusive(() =>
-      fetch(`http://${this.#host}/json/state`).then((res) => res.json())
-    );
+    fetch(`http://${this.#host}/json/state`).then((res) => res.json());
   }
 
   #applyState(state: WledState) {
-    return this.#mutex.runExclusive(() =>
-      fetch(`http://${this.#host}/json/state`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(state),
-      }).then((res) => res.json())
-    );
+    fetch(`http://${this.#host}/json/state`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(state),
+    }).then((res) => res.json());
   }
 
   async createEvent() {
-    const state = await this.#getState();
-    const key = this.#events.create(state);
+    return this.#mutex.runExclusive(async () => {
+      const state = await this.#getState();
+      const key = this.#events.create(state);
 
-    console.log("Event created", key, state);
+      console.log("Event created", key, state);
 
-    return key;
+      return key;
+    });
   }
 
   async deleteEvent(key: string) {
-    const state = this.#events.delete(key);
-    if (state) await this.#applyState(state);
+    return this.#mutex.runExclusive(async () => {
+      const state = this.#events.delete(key);
+      if (state) await this.#applyState(state);
 
-    console.log("Event deleted", key, state);
+      console.log("Event deleted", key, state);
 
-    return !!state;
+      return !!state;
+    });
   }
 
   async deleteAllEvents() {
-    const state = this.#events.deleteAllEvents();
-    if (state) await this.#applyState(state);
+    return this.#mutex.runExclusive(async () => {
+      const state = this.#events.deleteAllEvents();
+      if (state) await this.#applyState(state);
 
-    console.log("All events deleted", state);
+      console.log("All events deleted", state);
 
-    return !!state;
+      return !!state;
+    });
   }
 }
